a4d2c25f497ab2f838ca68b2137a38b50c8a17a6
CaheSquarizi <prof.cahe@hotmail.com>
2023-07-24 15:25:37 -0300

    ainda precisa mostrar array listaNumeros

--- src/somaDeArrays.java
+++ src/somaDeArrays.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 //gerador de números aleatórios.
 import java.util.Random;
 
@@ -10,7 +11,7 @@ public class somaDeArrays {
     public static void main(String[] args) {
 
         int limite = 10;
-        ArrayList<Integer> numeros = new ArrayList<Integer>(limite);
+        ArrayList<Integer> numeros = new ArrayList<>(limite);
         int total = 0;
         Random gerador = new Random(limite);
         int numeroGerado;
@@ -24,8 +25,25 @@ public class somaDeArrays {
                 total += numeros.get(i);
             }
         System.out.println("\nA soma dos números da lista é " + total);
+
+
+
+        System.out.println("\nAgora é sua vez! Digite 10 números para nossa lista.");
+
+            int[] listaNumeros = new int[limite];
+            Scanner lerNumero = new Scanner(System.in);
+            int num;
+            int cont = 0;
+            int novoTotal = 0;
+
+        while (cont < 10) {
+            System.out.println("Digite um número:");
+            num = lerNumero.nextInt();
+            listaNumeros[cont] = num;
+            novoTotal += num;
+            cont++;
+        }
+        lerNumero.close();
+       System.out.println("O novo valor total é : " + novoTotal);
     }
-
-
-
 }
